67a55d892a35e07c348afdca20da318864d3d0d1
Sarah Nickolai <sarahnickolai@gmail.com>
2014-04-06 22:38:22 -0400

    Added basic scene, capsule object moves back and forth over sphere, space background.

---
+++ 493proj/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	public float leftBound;
+	public float rightBound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xpos = transform.position.x;
+		xpos += speed*Time.deltaTime;
+		if (xpos > rightBound) 
+		{
+			xpos = rightBound;
+			speed = -Mathf.Abs(speed);
+		} 
+		else if (xpos < leftBound) 
+		{
+			xpos = leftBound;
+			speed = Mathf.Abs(speed);
+		}
+		//transform.rigidbody.velocity = new Vector3 (speed, 0, 0);
+		transform.position = new Vector3 (xpos, transform.position.y, transform.position.z);
+	}
+}
