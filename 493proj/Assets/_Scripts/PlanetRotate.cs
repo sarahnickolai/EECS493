89a158f5fc186f66326766166f42559b060ba0ad
Sarah Nickolai <sarahnickolai@gmail.com>
2014-04-07 19:56:53 -0400

    Added player movement via arrow keys, earth object, saucer object, asteroid object.

---
+++ 493proj/Assets/_Scripts/PlanetRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetRotate : MonoBehaviour {
+
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 center = transform.position + transform.localScale / 2;
+		transform.RotateAround (transform.position, new Vector3 (1, 1, 0), rotationSpeed);
+	}
+}
--- 493proj/Assets/_Scripts/PlayerMovement.cs
+++ 493proj/Assets/_Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour {
 	public float speed;
 	public float leftBound;
 	public float rightBound;
+	public float upperBound;
+	public float lowerBound;
 
 	// Use this for initialization
 	void Start () {
@@ -14,19 +16,60 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float xpos = transform.position.x;
-		xpos += speed*Time.deltaTime;
-		if (xpos > rightBound) 
-		{
-			xpos = rightBound;
-			speed = -Mathf.Abs(speed);
-		} 
-		else if (xpos < leftBound) 
-		{
-			xpos = leftBound;
-			speed = Mathf.Abs(speed);
-		}
+		//float xpos = transform.position.x;
+		//xpos += speed*Time.deltaTime;
+		//if (xpos > rightBound) 
+		//{
+		//	xpos = rightBound;
+		//	speed = -Mathf.Abs(speed);
+		//} 
+		//else if (xpos < leftBound) 
+		//{
+		//	xpos = leftBound;
+		//	speed = Mathf.Abs(speed);
+		//}
 		//transform.rigidbody.velocity = new Vector3 (speed, 0, 0);
-		transform.position = new Vector3 (xpos, transform.position.y, transform.position.z);
+		//transform.position = new Vector3 (xpos, transform.position.y, transform.position.z);
+	
+		float xpos = transform.position.x;
+		float ypos = transform.position.y;
+
+		if (Input.GetKey (KeyCode.UpArrow))
+		{
+			ypos += speed*Time.deltaTime;
+			if(ypos > upperBound)
+			{
+				ypos = upperBound;
+			}
+		} 
+
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			ypos -= speed*Time.deltaTime;
+			if(ypos < lowerBound)
+			{
+				ypos = lowerBound;
+			}
+		}
+
+
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			xpos += -speed*Time.deltaTime;
+			if(xpos < leftBound)
+			{
+				xpos = leftBound;
+			}
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			xpos += speed*Time.deltaTime;
+			if(xpos > rightBound)
+			{
+				xpos = rightBound;
+			}
+		}
+
+		transform.position = new Vector3 (xpos, ypos, transform.position.z);
 	}
 }
